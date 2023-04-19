1e5943e090cfc8a3b7a092700f420e193896a3a1
Liam <liam@scalzulli.com>
2023-04-19 16:59:29 -0400

    Add rich for terminal text formatting

--- pdfathom/repl.py
+++ pdfathom/repl.py
@@ -2,17 +2,21 @@ import atexit
 import os
 import readline
 from dataclasses import dataclass
+from textwrap import dedent
 from typing import Dict
 
 from langchain.chains.retrieval_qa.base import BaseRetrievalQA
+from rich import print
 
 @dataclass
 class Repl:
-  HELP = '''
+  HELP = dedent(
+    '''
     Enter a query to search the PDF, or type 'exit' to quit.
     Use the up and down arrow keys to navigate through command history.
     Press 'Tab' for command completion suggestions.
   '''
+  )
 
   def __init__(self, retrievers: Dict[str, BaseRetrievalQA]):
     self.retrievers = retrievers
@@ -21,7 +25,7 @@ class Repl:
   def run(self):
     self._init_readline()
 
-    print(f"Active PDF: {self.active}\n{Repl.HELP}")
+    print(f"Active document: [bold]{self.active}[/bold]\n{Repl.HELP}")
 
     while True:
       try:
@@ -33,12 +37,17 @@ class Repl:
         elif query.lower().startswith("switch"):
           _, pdf_name = query.split(" ", 1)
           if pdf_name in self.retrievers:
-            self.active = pdf_name
-            print(f"Switched to PDF: {self.active}")
+            print(f"Successfully switched to {self.active := pdf_name}")
           else:
-            print(f"PDF not found: {pdf_name}")
+            print(f"{pdf_name} does not exist")
         else:
-          print(self.retrievers[self.active]({"query": query})["result"])
+          print(
+            '\n' + dedent(
+              self.retrievers[self.active]({
+                "query": query
+              })["result"].strip()
+            ) + '\n'
+          )
       except KeyboardInterrupt:
         break
       except Exception as error:
